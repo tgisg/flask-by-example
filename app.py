b4d17b7eac33c8d3385b4cd3eae821f5c5a56efd
tgisg <soniagupta504@gmail.com>
2017-01-12 07:27:28 -0700

    wip debugging .env not setting variable

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -11,13 +11,10 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
